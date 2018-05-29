492fc279616bb99fae403d9b4edfd142a2dfb72e
VeronicaRX <36456784+VeronicaRX@users.noreply.github.com>
2018-05-29 15:33:43 +0200

    Delete constrained_vina.sh

--- constrained_vina.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pr4path="/Library/MGLTools/1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24"
-vinapath="/Users/veronicagueye/Desktop/autodock_vina_1_1_2_mac/bin"
-pythonpath="/Library/MGLTools/1.5.6/bin"
-babelpath="/usr/local/bin"
-
-${pythonpath}/pythonsh ${pr4path}/prepare_receptor4.py -r receptor.pdb -A bonds_hydrogens -U nphs_lps_waters_deleteAltB -o receptor.pdbqt
-${vinapath}/vina --receptor receptor.pdbqt --flex ligand_constrained.pdbqt  --ligand water.pdbqt --out ligand_constrained.pdbqt --log ligand_constrained.log --config vina.config
