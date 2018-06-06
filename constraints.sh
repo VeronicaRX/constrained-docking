b4c7b4e80091c70622eadef44f7fe157ef225a1a
VeronicaRX <36456784+VeronicaRX@users.noreply.github.com>
2018-06-06 11:59:47 +0200

    Update constraints.sh

--- constraints.sh
+++ constraints.sh
@@ -1,3 +1,4 @@
+#path to be modified
 #!/bin/bash
 vinapath="/Users/veronicagueye/Desktop/autodock_vina_1_1_2_mac/bin"
 babelpath="/usr/local/bin"
@@ -5,6 +6,7 @@ pr4path="/Library/MGLTools/1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24"
 pythonpath="/Library/MGLTools/1.5.6/bin"
 
 ${pythonpath}/pythonsh ${pr4path}/prepare_ligand4.py -l ligand.mol2 -o lk.pdbqt -R 5
+#lk.pdbqt output 
 
 ${vinapath}/vina --receptor receptor.pdbqt --flex flex_581_C3.pdbqt --ligand water.pdbqt --out C3.pdbqt --log C3.log --config vina.config 
 
